54776378406f40f2e152350eea2f5ada6f8cc2f7
hoojo <hoojo@qq.com>
2018-04-10 10:22:11 +0800

    :art: reflection examples

---
+++ src/java/main/com/cnblogs/hoojo/reflection/TypeTokenTest.java
@@ -0,0 +1,24 @@
+package com.cnblogs.hoojo.reflection;
+
+import org.junit.Test;
+
+import com.google.common.reflect.TypeToken;
+
+/**
+ * <b>function:</b>
+ * @author hoojo
+ * @createDate 2018年3月28日 下午6:37:46
+ * @file TypeTokenTest.java
+ * @package com.cnblogs.hoojo.reflection
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class TypeTokenTest {
+
+	@Test
+	public void test1() {
+		TypeToken<String> token = TypeToken.of(String.class);
+	}
+}
