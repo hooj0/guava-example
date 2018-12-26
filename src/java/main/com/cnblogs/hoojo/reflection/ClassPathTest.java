506a643df7c326f77b7bd6441b9125e65d88637d
hoojo <hoojo@qq.com>
2018-12-26 17:19:24 +0800

    :sparkles: :bento: :tada:  Init add 尽最大努力的类路径扫描

---
+++ src/java/main/com/cnblogs/hoojo/reflection/ClassPathTest.java
@@ -0,0 +1,46 @@
+package com.cnblogs.hoojo.reflection;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.google.common.reflect.ClassPath;
+
+/**
+ * 尽最大努力的类路径扫描
+ * 
+ * @author hoojo
+ * @createDate 2018年12月26日 下午5:12:19
+ * @file ClassPathTest.java
+ * @package com.cnblogs.hoojo.reflection
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class ClassPathTest {
+
+	@Test
+	public void test1() throws IOException {
+		ClassLoader classloader = ClassLoader.getSystemClassLoader();
+		ClassPath classPath = ClassPath.from(classloader); // 扫描类加载器使用的类路径
+		// 扫描指定包下面的class
+		for (ClassPath.ClassInfo classInfo : classPath.getTopLevelClasses("com.cnblogs.hoojo.functional")) {
+			out(classInfo.getName());
+		}
+		
+		// 会把所以class加载出来，包括jar文件中
+		for (ClassPath.ClassInfo classInfo : classPath.getAllClasses()) {
+			out(classInfo.getName());
+		}
+		
+		// 递归
+		for (ClassPath.ClassInfo classInfo : classPath.getTopLevelClassesRecursive("com.cnblogs.hoojo.functional")) {
+			out(classInfo.getName());
+		}
+	}
+	
+	private void out(Object o) {
+		System.out.println(o);
+	}
+}
