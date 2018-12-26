dbd202468e5a6bc386f7ef6c3376ea90c3ade120
hoojo <hoojo@qq.com>
2018-12-26 10:35:27 +0800

    :sparkles: :bento: :recycle:  Changed reflection type token

--- src/java/main/com/cnblogs/hoojo/reflection/TypeTokenTest.java
+++ src/java/main/com/cnblogs/hoojo/reflection/TypeTokenTest.java
@@ -1,6 +1,7 @@
 package com.cnblogs.hoojo.reflection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -34,6 +35,31 @@ public class TypeTokenTest {
 		System.out.println(stringList.getClass().isAssignableFrom(intList.getClass())); // true
 		//returns true, even though ArrayList<String> is not assignable from ArrayList<Integer>
 		
+		// 结论：
+		// 如果你在运行时有一个ArrayList<String>对象，
+		// 你不能够判定这个对象是有泛型类型ArrayList<String>的 —— 并且通过不安全的原始类型，
+		// 你可以将这个对象强制转换成ArrayList<Object>。
+		
+		// 反射允许你去检测方法和类的泛型类型。如果你实现了一个返回List的方法，
+		// 并且你用反射获得了这个方法的返回类型，你会获得代表List<String>的ParameterizedType。
+		
+		// TypeToken类使用这种变通的方法以最小的语法开销去支持泛型类型的操作。
 		TypeToken<String> token = TypeToken.of(String.class);
+	}
+	
+	@Test
+	public void test2() {
+		// 获取一个基本的、原始类的TypeToken非常简单：
+		TypeToken<String> stringTok = TypeToken.of(String.class);
+		
+		TypeToken<Integer> intTok = TypeToken.of(Integer.class);
+		
+		// 为获得一个含有泛型的类型的TypeToken —— 当你知道在编译时的泛型参数类型 —— 你使用一个空的匿名内部类：
+		TypeToken<List<String>> stringListToken = new TypeToken<List<String>>() {};
+		
+	}
+	
+	private void out(Object o) {
+		System.out.println(o);
 	}
 }
