2b67d572eb358e74c984a8a1734b097b31128f43
hoojo <hoojo@qq.com>
2019-01-02 17:09:33 +0800

    :sparkles: :bento: :tada:  Init add based test class

---
+++ src/java/main/com/cnblogs/hoojo/BasedTest.java
@@ -0,0 +1,19 @@
+package com.cnblogs.hoojo;
+
+/**
+ * based test class
+ * @author hoojo
+ * @createDate 2019年1月2日 下午4:25:38
+ * @file BasedTest.java
+ * @package com.cnblogs.hoojo
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class BasedTest {
+
+	protected void out(Object o) {
+		System.out.println(o);
+	}
+}
