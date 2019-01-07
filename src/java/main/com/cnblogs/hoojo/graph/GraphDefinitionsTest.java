030669c2a893df3b11331b8d7d6ae502fb607de9
hoojo <hoojo@qq.com>
2019-01-07 18:03:59 +0800

    :sparkles: :bento: :tada:  Init add 图的定义 graph defined

---
+++ src/java/main/com/cnblogs/hoojo/graph/GraphDefinitionsTest.java
@@ -0,0 +1,18 @@
+package com.cnblogs.hoojo.graph;
+
+import com.cnblogs.hoojo.BasedTest;
+
+/**
+ * 图的定义 graph defined
+ * @author hoojo
+ * @createDate 2019年1月7日 下午6:02:41
+ * @file GraphDefinitionsTest.java
+ * @package com.cnblogs.hoojo.graph
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class GraphDefinitionsTest extends BasedTest {
+
+}
