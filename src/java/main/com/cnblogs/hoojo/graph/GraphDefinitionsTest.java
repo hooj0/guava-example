c3e4b5a86b4f8aefb588a89b686b2678f8015a59
hoojo <hoojo@qq.com>
2019-01-09 16:14:44 +0800

    :sparkles: :bento: :recycle:  Changed 图的定义 graph defined

--- src/java/main/com/cnblogs/hoojo/graph/GraphDefinitionsTest.java
+++ src/java/main/com/cnblogs/hoojo/graph/GraphDefinitionsTest.java
@@ -4,6 +4,12 @@ import com.cnblogs.hoojo.BasedTest;
 
 /**
  * 图的定义 graph defined
+ * Guava库的目录common.graph包含的模块是一个描述实体(entity)以及实体之间的关系的图数据结构模型库。
+ * 
+ * 例如：网页与超链接、科学家与他们写的论文、机场与其航线、人与其家族等。Guava-Graph模块的目的是提供一种通用以及可扩展的语言来描述类似上述的举例。
+ * 
+ * https://www.jianshu.com/nb/20139843
+ * 
  * @author hoojo
  * @createDate 2019年1月7日 下午6:02:41
  * @file GraphDefinitionsTest.java
@@ -15,4 +21,6 @@ import com.cnblogs.hoojo.BasedTest;
  */
 public class GraphDefinitionsTest extends BasedTest {
 
+	public void testDefined() {
+	}
 }
