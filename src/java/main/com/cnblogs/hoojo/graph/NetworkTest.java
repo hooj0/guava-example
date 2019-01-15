6f3750f166cf7b73c8808639d25fbd4b22080344
hoojo <hoojo@qq.com>
2019-01-15 18:23:14 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>

---
+++ src/java/main/com/cnblogs/hoojo/graph/NetworkTest.java
@@ -0,0 +1,18 @@
+package com.cnblogs.hoojo.graph;
+
+import com.cnblogs.hoojo.BasedTest;
+
+/**
+ * <b>function:</b>
+ * @author hoojo
+ * @createDate 2019年1月15日 下午5:53:20
+ * @file NetworkTest.java
+ * @package com.cnblogs.hoojo.graph
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class NetworkTest extends BasedTest {
+
+}
