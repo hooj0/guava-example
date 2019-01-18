93a5d7babd69915cb8c79972ef6c7774efa1e827
hoojo <hoojo@qq.com>
2019-01-18 15:19:08 +0800

    :sparkles: :bento: :recycle:  Changed value graph test example

--- src/java/main/com/cnblogs/hoojo/graph/ValueGraphTest.java
+++ src/java/main/com/cnblogs/hoojo/graph/ValueGraphTest.java
@@ -1,7 +1,6 @@
 package com.cnblogs.hoojo.graph;
 
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.Test;
@@ -57,13 +56,13 @@ public class ValueGraphTest extends BasedTest {
 		// 节点类型为Integer,边类型为String
 		int NODE_COUNT = 100;
 		
-		MutableValueGraph<Integer, String> graph1 = ValueGraphBuilder.directed() //有向
+		MutableValueGraph<Integer, String> graph = ValueGraphBuilder.directed() //有向
 		    .allowsSelfLoops(true) //允许自环
 		    .expectedNodeCount(NODE_COUNT) //期望节点数
 		    .nodeOrder(ElementOrder.<Integer>insertion()) //节点顺序
 		    .build();
 
-		out("initlized graph: " + graph1); // initlized graph: isDirected: true, allowsSelfLoops: true, nodes: [], edges: {}
+		out("initlized graph: " + graph); // initlized graph: isDirected: true, allowsSelfLoops: true, nodes: [], edges: {}
 
 		Integer N1 = 1;
 		Integer N2 = 2;
@@ -77,28 +76,28 @@ public class ValueGraphTest extends BasedTest {
 		String E34 = "3-4";
 		String E44 = "4-4";
 		
-		graph1.putEdgeValue(N3, N1, E31);
-		graph1.putEdgeValue(N3, N4, E34);
-		graph1.putEdgeValue(N4, N4, E44);
-		graph1.putEdgeValue(N1, N1, E11);
-		graph1.putEdgeValue(N1, N2, E12);
-		graph1.putEdgeValue(N2, N1, E21);
-		graph1.putEdgeValue(N1, N3, E13);
+		graph.putEdgeValue(N3, N1, E31);
+		graph.putEdgeValue(N3, N4, E34);
+		graph.putEdgeValue(N4, N4, E44);
+		graph.putEdgeValue(N1, N1, E11);
+		graph.putEdgeValue(N1, N2, E12);
+		graph.putEdgeValue(N2, N1, E21);
+		graph.putEdgeValue(N1, N3, E13);
 
-		out("put edges after graph1: " + graph1); // put edges after graph1: isDirected: true, allowsSelfLoops: true, nodes: [3, 1, 4, 2], edges: {<3 -> 4>=3-4, <3 -> 1>=3-1, <1 -> 1>=1-1, <1 -> 2>=1-2, <1 -> 3>=1-3, <4 -> 4>=4-4, <2 -> 1>=2-1}
+		out("put edges after graph: " + graph); // put edges after graph: isDirected: true, allowsSelfLoops: true, nodes: [3, 1, 4, 2], edges: {<3 -> 4>=3-4, <3 -> 1>=3-1, <1 -> 1>=1-1, <1 -> 2>=1-2, <1 -> 3>=1-3, <4 -> 4>=4-4, <2 -> 1>=2-1}
 
 
 		//返回图中所有的节点(顺序依赖nodeOrder)
-		Set<Integer> nodes = graph1.nodes(); 
-		out("graph1 nodes count:" + nodes.size() + ", nodes value:" + format(nodes)); // graph1 nodes count:4, nodes value:3,1,4,2,
+		Set<Integer> nodes = graph.nodes(); 
+		out("graph nodes count:" + nodes.size() + ", nodes value:" + format(nodes)); // graph nodes count:4, nodes value:3,1,4,2,
 
 		// 获取连接边：
-		String edge = graph1.edgeValueOrDefault(N1, N2, "@null");
+		String edge = graph.edgeValueOrDefault(N1, N2, "@null");
 		out("graph node " + N1 + " & " + N2 + " edge: " + edge); // graph node 1 & 2 edge: 1-2
 
 		// asGraph()转换为Graph视图
 		// asGraph()实际上是重新new了一个AbstractGraph，只是它的接口实现是调用了Graph本身的接口，因此如果修改asGraph()返回的视图的数据，其变化也会反映在Graph本身上，反之亦然。
-		Graph<Integer> graph5 = graph1.asGraph();
+		Graph<Integer> graph5 = graph.asGraph();
 		out("asGraph:" + graph5); // asGraph:isDirected: true, allowsSelfLoops: true, nodes: [3, 1, 4, 2], edges: [<3 -> 4>, <3 -> 1>, <1 -> 1>, <1 -> 2>, <1 -> 3>, <4 -> 4>, <2 -> 1>]
 
 	}
@@ -108,16 +107,6 @@ public class ValueGraphTest extends BasedTest {
 	    for (Object value : collections) {
 	        builder.append(value);
 	        builder.append(",");
-	    }
-	    return builder.toString();
-	}
-	
-	private String format(Iterable<?> iterable) {
-	    StringBuilder builder = new StringBuilder();
-	    Iterator<?> iter = iterable.iterator();
-	    while (iter.hasNext()) {
-	    	 builder.append(iter.next());
-		        builder.append(",");
 	    }
 	    return builder.toString();
 	}
