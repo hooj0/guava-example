094cf0eb473dc2c3aa2cf01731e81677f21007ff
hoojo <hoojo@qq.com>
2019-01-21 18:21:41 +0800

    :sparkles: :bento: :recycle:  Changed value graph test example

--- src/java/main/com/cnblogs/hoojo/graph/ValueGraphTest.java
+++ src/java/main/com/cnblogs/hoojo/graph/ValueGraphTest.java
@@ -1,5 +1,6 @@
 package com.cnblogs.hoojo.graph;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.graph.ElementOrder;
+import com.google.common.graph.EndpointPair;
 import com.google.common.graph.Graph;
 import com.google.common.graph.MutableValueGraph;
 import com.google.common.graph.Traverser;
@@ -159,8 +161,28 @@ public class ValueGraphTest extends AbstractGraphTests {
 		out("ves: " + format(ves)); // ves: {v6:7, v7:16, v8:14, v9:18, v1:0, v2:6, v3:4, v4:5, v5:7, }
 
 		// 递推求得vl(j)值
-		ves = getVlValues(graph, topologicallys, ves);
-		out("ves: " + format(ves)); // ves: {v6:10, v7:16, v8:14, v9:18, v1:0, v2:6, v3:6, v4:8, v5:7, }
+		Map<String, Integer> vls = getVlValues(graph, topologicallys, ves);
+		out("vls: " + format(vls)); // vls: {v6:10, v7:16, v8:14, v9:18, v1:0, v2:6, v3:6, v4:8, v5:7, }
+		
+		// 根据前面求取的ve(j)和vl(j)来找出关键活动（判断条件：ve(j) == vl(k) - dut(<j,k>)）：
+		/**
+		 * 判断条件：ve(j) == vl(k) - dut(<j,k>)
+		 */
+		//关键活动路径结果集
+		List<EndpointPair<String>> criticalActives = new ArrayList<>(); 
+		//返回图中所有活动(边)
+		Set<EndpointPair<String>> edgs = graph.edges(); 
+		//遍历每一条边（活动），过滤出：ve(j) == vl(k) - dut<j, k>
+		for (EndpointPair<String> endpoint : edgs) {
+		    final int dut = graph.edgeValueOrDefault(endpoint.nodeU(), endpoint.nodeV(), 0);
+		    //ve(j) == vl(k) - dut<j, k>
+		    if (vls.get(endpoint.nodeV()) - dut == ves.get(endpoint.nodeU())) { 
+		        criticalActives.add(endpoint);
+		    }
+		}
+		out("critical actives: " + format(criticalActives)); // critical actives: <v1 -> v2>,<v2 -> v5>,<v5 -> v7>,<v5 -> v8>,<v7 -> v9>,<v8 -> v9>,
+
+
 	}
 	
 	/**
