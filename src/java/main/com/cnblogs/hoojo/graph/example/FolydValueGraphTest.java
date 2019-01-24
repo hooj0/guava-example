e564e9afea4982c0b0edb3be06f5caaf092cb44b
hoojo <hoojo@qq.com>
2019-01-24 14:28:21 +0800

    :sparkles: :bento: :tada:  Init add Floyd（弗洛伊德）算法

---
+++ src/java/main/com/cnblogs/hoojo/graph/example/FolydValueGraphTest.java
@@ -0,0 +1,29 @@
+package com.cnblogs.hoojo.graph.example;
+
+import org.junit.Test;
+
+import com.cnblogs.hoojo.graph.AbstractGraphTests;
+
+/**
+ * Floyd（弗洛伊德）算法
+ * 
+ * Floyd算法是一个经典的动态规划算法。是解决任意两点间的最短路径 (称为多源最短路径问题)的一种算法，
+ * 可以正确处理有向图或负权的最短路径问题。
+ * （动态规划算法是通过拆分问题规模，并定义问题状态与状态的关系，使得问题能够以递推（分治）的方式去解决，
+ * 最终合并各个拆分的小问题的解为整个问题的解。）
+ * @author hoojo
+ * @createDate 2019年1月24日 上午10:48:02
+ * @file FolydValueGraphTest.java
+ * @package com.cnblogs.hoojo.graph.example
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class FolydValueGraphTest extends AbstractGraphTests {
+
+	@Test
+	public void test() {
+		
+	}
+}
