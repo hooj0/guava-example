9956881e80237c5a702a16d0dd020d8265222c3f
hoojo <hoojo@qq.com>
2019-01-24 18:15:47 +0800

    :sparkles: :bento: :tada:  Init add Kruskal（克鲁斯卡尔）算法

---
+++ src/java/main/com/cnblogs/hoojo/graph/example/KruskalValueGraphTest.java
@@ -0,0 +1,27 @@
+package com.cnblogs.hoojo.graph.example;
+
+import org.junit.Test;
+
+import com.cnblogs.hoojo.graph.AbstractGraphTests;
+
+/**
+ * Kruskal（克鲁斯卡尔）算法
+ * 该算法也称为“加边法”，初始时最小生成树的边数为0，
+ * 每迭代一次选择一条满足条件的最小代价边加入到最小生成树的边集合中。
+ * 
+ * @author hoojo
+ * @createDate 2019年1月24日 下午5:41:47
+ * @file KruskalValueGraphTest.java
+ * @package com.cnblogs.hoojo.graph.example
+ * @project guava-example
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class KruskalValueGraphTest extends AbstractGraphTests {
+
+	@Test
+	public void test() {
+		
+	}
+}
