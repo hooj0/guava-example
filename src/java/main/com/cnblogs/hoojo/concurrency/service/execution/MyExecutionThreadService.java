20d1f10d5288400e366dc7d10655e9120969668e
hoojo <hoojo@qq.com>
2021-06-22 18:01:57 +0800

    :sparkles: :bento: :recycle:  Changed 通过单线程处理启动、运行、和关闭等操作

--- src/java/main/com/cnblogs/hoojo/concurrency/service/execution/MyExecutionThreadService.java
+++ src/java/main/com/cnblogs/hoojo/concurrency/service/execution/MyExecutionThreadService.java
@@ -14,6 +14,7 @@ import com.google.common.util.concurrent.AbstractExecutionThreadService;
  * @email hoojo_@126.com
  * @version 1.0
  */
+@SuppressWarnings("ALL")
 public class MyExecutionThreadService extends AbstractExecutionThreadService {
 
 	@Override
