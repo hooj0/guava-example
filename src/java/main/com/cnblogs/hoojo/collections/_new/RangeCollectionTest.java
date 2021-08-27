e6802b53b0a814de7efed2415c45d11e2fc20c59
hoojo <hoojo@qq.com>
2021-08-27 21:27:58 +0800

    :sparkles: :bento: :recycle:  Changed RangeSet/RangeMap

--- src/java/main/com/cnblogs/hoojo/collections/_new/RangeCollectionTest.java
+++ src/java/main/com/cnblogs/hoojo/collections/_new/RangeCollectionTest.java
@@ -1,5 +1,6 @@
 package com.cnblogs.hoojo.collections._new;
 
+import com.cnblogs.hoojo.BasedTest;
 import org.junit.Test;
 
 import com.google.common.collect.BoundType;
@@ -20,7 +21,8 @@ import com.google.common.collect.TreeRangeSet;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public class RangeCollectionTest {
+@SuppressWarnings("ALL")
+public class RangeCollectionTest extends BasedTest {
 
 	@Test
 	public void testRangeSet() {
@@ -32,25 +34,25 @@ public class RangeCollectionTest {
 		tree.add(Range.openClosed(10, 13));
 		tree.add(Range.closedOpen(15, 20));
 		
-		System.out.println(tree); // [(2..13], [15..20)]
-		System.out.println(tree.encloses(Range.open(5, 10))); // true 存在某一区间
-		System.out.println(tree.encloses(Range.open(5, 14))); // false
+		out(tree); // [(2..13], [15..20)]
+		out(tree.encloses(Range.open(5, 10))); // true 存在某一区间
+		out(tree.encloses(Range.open(5, 14))); // false
 		
-		System.out.println(tree.intersects(Range.open(50, 188))); // false 只要和边界有交集就为true
-		System.out.println(tree.intersects(Range.open(14, 188))); // true
+		out(tree.intersects(Range.open(50, 188))); // false 只要和边界有交集就为true
+		out(tree.intersects(Range.open(14, 188))); // true
 		
-		System.out.println(tree.asDescendingSetOfRanges()); // [[15..20), (2..13]] 倒序集合区间
-		System.out.println(tree.asRanges()); // [(2..13], [15..20)]
-		System.out.println(tree.complement()); // [(-∞..2], (13..15), [20..+∞)] 为重合区间边界
+		out(tree.asDescendingSetOfRanges()); // [[15..20), (2..13]] 倒序集合区间
+		out(tree.asRanges()); // [(2..13], [15..20)]
+		out(tree.complement()); // [(-∞..2], (13..15), [20..+∞)] 为重合区间边界
 		
-		System.out.println(tree.rangeContaining(22)); // null 返回该数值所在区间
-		System.out.println(tree.rangeContaining(10)); // (2..13]
+		out(tree.rangeContaining(22)); // null 返回该数值所在区间
+		out(tree.rangeContaining(10)); // (2..13]
 		
-		System.out.println(tree.span()); // (2..20) 最小-最大区间边界
-		System.out.println(tree.subRangeSet(Range.open(5, 55))); // [(5..13], [15..20)] 返回区间的交集区间
+		out(tree.span()); // (2..20) 最小-最大区间边界
+		out(tree.subRangeSet(Range.open(5, 55))); // [(5..13], [15..20)] 返回区间的交集区间
 		
 		ImmutableRangeSet<Integer> range = ImmutableRangeSet.<Integer>of(Range.open(2, 10));
-		System.out.println(range); // [(2..10)]
+		out(range); // [(2..10)]
 	}
 	
 	@Test 
@@ -61,20 +63,20 @@ public class RangeCollectionTest {
 		range.put(Range.closed(7, 9), "t00");
 		range.put(Range.closed(17, 22), "bar");
 		
-		System.out.println(range); // [[3..7)=foo, [7..9]=t00, [17..22]=bar]
-		System.out.println(range.asMapOfRanges()); // {[3..7)=foo, [7..9]=t00, [17..22]=bar}
+		out(range); // [[3..7)=foo, [7..9]=t00, [17..22]=bar]
+		out(range.asMapOfRanges()); // {[3..7)=foo, [7..9]=t00, [17..22]=bar}
 		// 匹配到key的对应的区间
-		System.out.println(range.getEntry(8)); // [7..9]=t00
+		out(range.getEntry(8)); // [7..9]=t00
 	}
 	
 	@Test
 	public void testRange() {
-		System.out.println(Range.all()); // (-∞..+∞)
-		System.out.println(Range.atLeast(5)); // 上限 [5..+∞)
-		System.out.println(Range.atMost(5)); // 下限  (-∞..5]
+		out(Range.all()); // (-∞..+∞)
+		out(Range.atLeast(5)); // 上限 [5..+∞)
+		out(Range.atMost(5)); // 下限  (-∞..5]
 		
-		System.out.println(Range.downTo(4, BoundType.CLOSED)); // [4..+∞)
-		System.out.println(Range.greaterThan(9)); // (9..+∞)
-		System.out.println(Range.singleton(3)); // [3..3] 单区间
+		out(Range.downTo(4, BoundType.CLOSED)); // [4..+∞)
+		out(Range.greaterThan(9)); // (9..+∞)
+		out(Range.singleton(3)); // [3..3] 单区间
 	}
 }
